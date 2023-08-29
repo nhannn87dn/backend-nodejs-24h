3d991b9fadbc2e9a5ebb7917e4ac6a605b5405f7
Nhan <nhannn87dn@gmail.com>
2023-08-29 11:23:04 +0700

    m

--- 02-Examples/SIMPLE-express-TypeORM/src/models/user.model.js
+++ 02-Examples/SIMPLE-express-TypeORM/src/models/user.model.js
@@ -1,24 +1,32 @@
-const { Entity, PrimaryGeneratedColumn, Column } = require('typeorm');
+const { EntitySchema } = require('typeorm');
 
-@Entity()
-class User {
-  @PrimaryGeneratedColumn()
-  id;
-
-  @Column()
-  firstName;
-
-  @Column()
-  lastName;
-
-  @Column({ unique: true })
-  email;
-
-  @Column({ unique: true })
-  phoneNumber;
-
-  @Column()
-  password;
-}
+const User = new EntitySchema({
+  name: 'User',
+  tableName: 'users',
+  columns: {
+    id: {
+      primary: true,
+      type: 'int',
+      generated: true,
+    },
+    firstName: {
+      type: 'varchar',
+    },
+    lastName: {
+      type: 'varchar',
+    },
+    email: {
+      type: 'varchar',
+      unique: true,
+    },
+    phoneNumber: {
+      type: 'varchar',
+      unique: true,
+    },
+    password: {
+      type: 'varchar',
+    },
+  },
+});
 
 module.exports = User;
