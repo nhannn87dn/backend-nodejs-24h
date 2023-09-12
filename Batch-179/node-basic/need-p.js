dd154e8f0f4d1ec84d62cf4f74ffce43a8d7ad86
nhan <nhannn87dn@gmail.com>
2023-09-12 19:24:35 +0700

    up

--- Batch-179/node-basic/need-p.js
+++ Batch-179/node-basic/need-p.js
@@ -1,24 +1,19 @@
-//Mô phỏng trả về một mảng users từ Database
-function getUsers() {
-    let users = [];
-    //Sử dụng setTimeout để Delay 3 giây
-    setTimeout(() => {
-      users = [
-        { username: 'john', email: 'john@test.com' },
-        { username: 'jane', email: 'jane@test.com' },
-      ];
-    }, 3000);
-    return users;
-  }
-  // Định nghĩa hàm Tìm user có tên john
-  function findUser(username) {
-    const users = getUsers(); //3s sau no moi co dc ket qua
-    const user = users.find((user) => user.username === username);
-    return user;
-  }
-  //Gọi hàm 
-  console.log(findUser('john'));
-  
-  //Kết quả
-  //{ username: 'john', email: 'john@test.com' }
-  
+
+const getUsers = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const users = [
+      { username: 'john', email: 'john@test.com' },
+      { username: 'jane', email: 'jane@test.com' },
+    ];
+    resolve(users); // Trả về mảng users
+  }, 3000);
+});
+
+async function findUser(username) {
+  const users = await getUsers();
+  const user = users.find((user) => user.username === username);
+  return user;
+}
+
+findUser('john')
+.then((user) =>console.log(user))
